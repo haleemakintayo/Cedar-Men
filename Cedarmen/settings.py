a63bc26edde1ccb3d32399677dd5d72c7fe09fd5
Haleem Akintayo <127564674+haleemakintayo@users.noreply.github.com>
2025-12-22 21:31:25 +0100

    fix deploy

--- Cedarmen/settings.py
+++ Cedarmen/settings.py
@@ -134,19 +134,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+# Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-
-WHITENOISE_MANIFEST_STRICT = False
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+# CRITICAL FIX: Use the non-manifest storage to avoid the "MissingFileError" crash
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
