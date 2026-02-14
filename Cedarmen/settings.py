38a6ae8183ab6a8a9a38048fb5e34a9ce10fef7b
Daramola Samuel Pelumi <159028103+Qwerty3025@users.noreply.github.com>
2026-02-14 11:06:18 -0800

    Pelu pushed

--- Cedarmen/settings.py
+++ Cedarmen/settings.py
@@ -95,11 +95,13 @@ WSGI_APPLICATION = 'Cedarmen.wsgi.application'
 #     )
 # }
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600
+    )
 }
 
 
