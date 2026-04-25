fbba848da6b8494859622c5235f328d83422937e
Haleem Akintayo <127564674+haleemakintayo@users.noreply.github.com>
2026-04-25 14:49:40 +0100

    Add email field to checkout form and template
    Introduce an EmailField to CheckoutForm (with EmailInput and Bootstrap 'form-control' class) and render it in the checkout template between last name and address. This adds email capture to the checkout flow for contact/confirmation purposes.

--- orders/forms.py
+++ orders/forms.py
@@ -11,6 +11,7 @@ class CheckoutForm(forms.Form):
     
     first_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}))
     last_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
     address = forms.CharField(max_length=250, widget=forms.TextInput(attrs={'class': 'form-control'}))
     city = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
     state = forms.CharField(max_length=100, required=False, widget=forms.TextInput(attrs={'class': 'form-control'}))
